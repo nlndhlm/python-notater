2bb1dd23922bc6860a2fea1279f0aff6c94292dc
nlndhlm <30705699+nlndhlm@users.noreply.github.com>
2021-11-24 14:47:23 +0100

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -6,7 +6,7 @@ print(tall)
 
 
 # EKSEMPEL 2:
-# generere liste med partall fom 0 til men ikke med 20
+# generere liste med partall fom 0 til men ikke med 20:
 partall = [x for x in range(0, 20) if x %2 == 0]
 
 print(partall)
